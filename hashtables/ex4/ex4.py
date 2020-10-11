82eb7e6d12d901197b960907e766a5bc35f5bba8
Michelle Scott <scottmm374@gmail.com>
2020-10-11 12:29:03 -0400

    ex4 almost working, need to compare before append

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,25 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
+    # compare_dict = {}
+    result = []
+    # neg_dict = {}
+
+    for neg in range(len(a)):
+        if a[neg] < 0:
+            negative_num = abs(a[neg])
+
+        # TODO its not comparing correctly. Put a neg number in that did not have positive comparison.
+            if negative_num not in result:
+                result.append(negative_num)
+
+        # if neg not in compare_dict:
+        #     compare_dict[negative_num] = neg
+
+    # print(compare_dict)
+    print(result)
     return result
 
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+    print(has_negatives([1, 2, 3, -4]))
